ac8055c320f2fd5210a023f7163a5e489dfe2c2f
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-13 19:57:23 -0400

    Realizando lael DAO de Prioridad

---
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/local/dao/PrioridadDao.kt
@@ -0,0 +1,37 @@
+package edu.ucne.composedemo.mainsystemtickets.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Upsert
+import edu.ucne.composedemo.mainsystemtickets.data.local.entity.PrioridadEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PrioridadDao {
+    @Upsert
+    suspend fun save(prioridad: PrioridadEntity)
+
+    @Query(
+        """
+        SELECT * 
+        FROM Prioridades 
+        WHERE prioridadId=:id  
+        LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): PrioridadEntity?
+
+    @Delete
+    suspend fun delete(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM Prioridades")
+    fun getAll(): Flow<List<PrioridadEntity>>
+
+    @Query("SELECT diasCompromiso FROM Prioridades")
+    suspend fun getAllDias(): List<Int>
+
+    @Update
+    suspend fun update(updatedPrioridad: PrioridadEntity)
+}
