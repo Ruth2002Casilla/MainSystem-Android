e9ba35e271fa21bf6827373239a4fae40fe25ea8
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-13 21:29:55 -0400

    Realizando TicketRepository

---
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/repository/TicketRepository.kt
@@ -0,0 +1,21 @@
+package edu.ucne.composedemo.mainsystemtickets.data.repository
+
+import edu.ucne.composedemo.mainsystemtickets.data.local.dao.ClienteDao
+import edu.ucne.composedemo.mainsystemtickets.data.local.dao.TicketDao
+import edu.ucne.composedemo.mainsystemtickets.data.local.entity.ClienteEntity
+import edu.ucne.composedemo.mainsystemtickets.data.local.entity.TicketEntity
+import javax.inject.Inject
+
+class TicketRepository @Inject constructor(
+    private val ticketDao: TicketDao
+) {
+    suspend fun save(ticket: TicketEntity) = ticketDao.save(ticket)
+
+    suspend fun getTicket(id: Int) = ticketDao.find(id)
+
+    suspend fun delete(ticket: TicketEntity) = ticketDao.delete(ticket)
+
+    fun getTickets() = ticketDao.getAll()
+}
+
+
