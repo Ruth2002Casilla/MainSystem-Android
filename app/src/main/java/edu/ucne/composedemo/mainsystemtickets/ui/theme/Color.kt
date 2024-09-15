99e80f8102814c5c02648625ae7df717601f6dba
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-15 17:07:32 -0400

    Realizando el ViewModels

--- app/src/main/java/edu/ucne/composedemo/mainsystemtickets/ui/theme/Color.kt
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val bluecustom = Color(0xFF000A5F)
+val Black = Color(0xFF020202)
