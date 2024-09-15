e81692c1ecb5b5f93d873372a282e9867f5d9ec4
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-15 17:05:00 -0400

    Recursos

--- app/src/main/java/edu/ucne/composedemo/mainsystemtickets/MainActivity.kt
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/MainActivity.kt
@@ -4,44 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.aarondeveloper.ticketing.presentation.navigation.NavigationNavHost
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.composedemo.mainsystemtickets.data.local.database.QuikFixDb
 import edu.ucne.composedemo.mainsystemtickets.ui.theme.MainSystemTicketsTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var quikFixDb: QuikFixDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        quikFixDb = Room.databaseBuilder(
+            applicationContext,
+            QuikFixDb::class.java,
+            "QuikFixDb.db"
+        ).build()
+
         enableEdgeToEdge()
+
+        val prioridadDao = quikFixDb.prioridadDao()
+
         setContent {
             MainSystemTicketsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+
+                NavigationNavHost(
+                    navHostController = navController,
+                    PrioridadesLista = prioridadDao.getAll(),
+                    prioridadDao = prioridadDao
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MainSystemTicketsTheme {
-        Greeting("Android")
     }
 }
