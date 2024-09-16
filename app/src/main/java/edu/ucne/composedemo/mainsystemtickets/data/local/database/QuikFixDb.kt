34717191f768d9b04fec063fc4aa15273dc3f697
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-15 20:45:00 -0400

    Actualizando la base de datos

--- app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/local/database/QuikFixDb.kt
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/local/database/QuikFixDb.kt
@@ -3,15 +3,19 @@ package edu.ucne.composedemo.mainsystemtickets.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.composedemo.mainsystemtickets.data.local.dao.PrioridadDao
+import edu.ucne.composedemo.mainsystemtickets.data.local.dao.SistemaDao
 import edu.ucne.composedemo.mainsystemtickets.data.local.entities.PrioridadEntity
+import edu.ucne.composedemo.mainsystemtickets.data.local.entities.SistemaEntity
 
 @Database(
     entities = [
         PrioridadEntity::class,
+        SistemaEntity::class
     ],
     version = 2,
     exportSchema = false
 )
 abstract class QuikFixDb : RoomDatabase() {
     abstract fun prioridadDao(): PrioridadDao
+    abstract fun sistemaDao(): SistemaDao
 }
