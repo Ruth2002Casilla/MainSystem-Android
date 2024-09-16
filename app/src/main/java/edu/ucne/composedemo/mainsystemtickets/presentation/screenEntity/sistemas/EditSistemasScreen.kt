c428a69b3a052c38e940698e425165870dffd5b1
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-15 20:46:55 -0400

    Realizando Edit

---
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/presentation/screenEntity/sistemas/EditSistemasScreen.kt
@@ -0,0 +1,230 @@
+package edu.ucne.composedemo.mainsystemtickets.presentation.screenEntity.sistemas
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.composedemo.mainsystemtickets.R
+import edu.ucne.composedemo.mainsystemtickets.ui.theme.Black
+import edu.ucne.composedemo.mainsystemtickets.ui.theme.bluecustom
+
+@Composable
+fun EditSistemasScreen(
+    viewModel: SistemaViewModel = hiltViewModel(),
+    sistemaId: Int?,
+    onDrawerToggle: () -> Unit,
+    goToSistema: () -> Unit
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    LaunchedEffect(key1 = true) {
+        if (sistemaId != null) {
+            viewModel.selectedSistema(sistemaId)
+        }
+    }
+
+    BodyEditSistemas(
+        uiState = uiState,
+        onNombreChange = viewModel::onNombreChange,
+        onDrawerToggle = onDrawerToggle,
+        goToSistema = goToSistema,
+        updateSistema = viewModel::update
+    )
+}
+
+@Composable
+fun BodyEditSistemas(
+    uiState: UiState,
+    onDrawerToggle: () -> Unit,
+    onNombreChange: (String) -> Unit,
+    goToSistema: () -> Unit,
+    updateSistema: () -> Unit
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.mipmap.sistemas),
+            contentDescription = "Background Principal",
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(top = 50.dp)
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .border(3.dp, bluecustom, shape = RoundedCornerShape(30.dp)),
+                colors = CardDefaults.cardColors(
+                    containerColor = White
+                ),
+                shape = RoundedCornerShape(30.dp),
+                elevation = CardDefaults.cardElevation(8.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 10.dp)
+                            .border(BorderStroke(2.dp, bluecustom), shape = RoundedCornerShape(10.dp))
+                    ) {
+                        BasicTextField(
+                            value = uiState.nombre ?: "",
+                            onValueChange = onNombreChange,
+                            textStyle = TextStyle(
+                                color = Black,
+                                fontSize = 16.sp
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 22.dp)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, top = 8.dp)
+                        ) {
+                            this@Column.AnimatedVisibility(
+                                visible = uiState.nombre.isNullOrEmpty(),
+                                enter = fadeIn(),
+                                exit = fadeOut(),
+                                modifier = Modifier
+                                    .offset(y = 14.dp)
+                            ) {
+                                Text(
+                                    text = "Nombre del Sistema",
+                                    color = Color.Gray,
+                                    fontSize = 16.sp
+                                )
+                            }
+                        }
+                    }
+
+                    uiState.errorMessage?.let { message ->
+                        Text(
+                            text = message,
+                            color = Color.Red,
+                            fontSize = 14.sp,
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                    }
+
+                    Button(
+                        onClick = {
+                            updateSistema()
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = bluecustom
+                        ),
+                        shape = RoundedCornerShape(10.dp)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "Actualizar",
+                                tint = White,
+                                modifier = Modifier.size(24.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = "Actualizar",
+                                color = White,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 18.sp
+                            )
+                        }
+                    }
+                }
+            }
+            LaunchedEffect(uiState.guardado) {
+                if (uiState.guardado == true) {
+                    goToSistema()
+                }
+            }
+        }
+    }
+}
+
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun EditPrioridadesScreenPreview() {
+    val testUiState = UiState(
+        nombre = "",
+        errorMessage = null,
+        guardado = false
+    )
+
+    val onDrawerToggle: () -> Unit = {}
+    val onNombreChange: (String) -> Unit = {}
+    val goToSistema: () -> Unit = {}
+    val updateSistema: () -> Unit = {}
+
+    BodyEditSistemas(
+        uiState = testUiState,
+        onDrawerToggle = onDrawerToggle,
+        onNombreChange = onNombreChange,
+        goToSistema = goToSistema,
+        updateSistema = updateSistema
+    )
+}
