62730bd2e4d5911359a03ebeb92a8b1ac1baeab5
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-15 23:42:53 -0400

    Cambios para cliente

--- app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/local/database/QuikFixDb.kt
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/local/database/QuikFixDb.kt
@@ -2,20 +2,24 @@ package edu.ucne.composedemo.mainsystemtickets.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.composedemo.mainsystemtickets.data.local.dao.ClienteDao
 import edu.ucne.composedemo.mainsystemtickets.data.local.dao.PrioridadDao
 import edu.ucne.composedemo.mainsystemtickets.data.local.dao.SistemaDao
+import edu.ucne.composedemo.mainsystemtickets.data.local.entities.ClienteEntity
 import edu.ucne.composedemo.mainsystemtickets.data.local.entities.PrioridadEntity
 import edu.ucne.composedemo.mainsystemtickets.data.local.entities.SistemaEntity
 
 @Database(
     entities = [
         PrioridadEntity::class,
-        SistemaEntity::class
+        SistemaEntity::class,
+        ClienteEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class QuikFixDb : RoomDatabase() {
     abstract fun prioridadDao(): PrioridadDao
     abstract fun sistemaDao(): SistemaDao
+    abstract fun clienteDao(): ClienteDao
 }
