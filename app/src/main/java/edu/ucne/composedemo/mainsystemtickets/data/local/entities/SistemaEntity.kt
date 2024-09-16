63e609641ebc5d4684422a48dd5a9e259db75228
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-09-15 20:42:10 -0400

    Realizando la entidad Sistema

---
+++ app/src/main/java/edu/ucne/composedemo/mainsystemtickets/data/local/entities/SistemaEntity.kt
@@ -0,0 +1,11 @@
+package edu.ucne.composedemo.mainsystemtickets.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Sistemas")
+data class SistemaEntity (
+    @PrimaryKey(autoGenerate = true)
+    val sistemaId: Int? = null,
+    var nombre: String = ""
+)
